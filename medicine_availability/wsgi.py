104d79b4a42a8b1e9fc9540ed56d5c0762bd8cb0
ajeeta <ajitap29@gmail.com>
2023-12-13 16:33:05 +0530

    deployement changes

--- medicine_availability/wsgi.py
+++ medicine_availability/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicine_availability.settings')
+#new code start here .........
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# new code ends here
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicine_availability.settings')
 
 application = get_wsgi_application()
